9d53fbb10b8de3b728a57f1a42d92e9310a039ca
Soraya de Ruiter <sorayamr91@gmail.com>
2023-07-09 12:47:07 +0200

    Basic first setup Flask, json, html and css

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask import render_template
+import json
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    with open("blog_posts.json", "r") as read_file:
+        blog_posts = json.load(read_file)
+    return render_template('index.html', posts=blog_posts)
+
 
 
 if __name__ == '__main__':
