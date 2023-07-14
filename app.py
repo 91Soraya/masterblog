ba11763043dced0d0264df730770e263fd93e59b
Soraya de Ruiter <sorayamr91@gmail.com>
2023-07-14 22:22:38 +0200

    Update function added

--- app.py
+++ app.py
@@ -5,17 +5,29 @@ import json
 app = Flask(__name__)
 
 
-def create_new_id():
+def get_all_posts():
     with open("blog_posts.json", "r") as read_file:
-        blog_posts = json.load(read_file)
-        new_id = blog_posts[-1]["id"] + 1
-        return new_id
+        return json.load(read_file)
+
+
+def create_new_id():
+    blog_posts = get_all_posts()
+    new_id = blog_posts[-1]["id"] + 1
+    return new_id
+
+
+def fetch_post_by_id(post_id):
+    blog_posts = get_all_posts()
+    fetched_post = {}
+    for blog in blog_posts:
+        if blog["id"] == post_id:
+            fetched_post = blog
+    return fetched_post
 
 
 @app.route('/')
 def index():
-    with open("blog_posts.json", "r") as read_file:
-        blog_posts = json.load(read_file)
+    blog_posts = get_all_posts()
     return render_template('index.html', posts=blog_posts)
 
 
@@ -47,6 +59,7 @@ def add():
         return redirect(url_for("index"))
     return render_template("add.html")
 
+
 @app.route('/delete/<int:post_id>', methods=['GET', 'POST'])
 def delete(post_id):
     updated_posts = []
@@ -64,6 +77,31 @@ def delete(post_id):
     return redirect(url_for('index'))
 
 
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+def update(post_id):
+    post_to_update = fetch_post_by_id(post_id)
+    if post_to_update is None:
+        return "Post not found", 404
+
+    if request.method == 'POST':
+        updated_posts = []
+        blog_posts = get_all_posts()
+        content = request.form.get('content')
+        title = request.form.get('title')
+        author = request.form.get('author')
+        for blog in blog_posts:
+            if blog['id'] == post_id:
+                blog['title'] = title
+                blog['author'] = author
+                blog['content'] = content
+            updated_posts.append(blog)
+
+        with open("blog_posts.json", "w") as write_file:
+            json.dump(updated_posts, write_file)
+
+        return redirect(url_for('index'))
+
+    return render_template('update.html', post=post_to_update)
 
 
 if __name__ == '__main__':
