0c4884937e408a7948c9a135879ce58a211f7a39
Soraya de Ruiter <sorayamr91@gmail.com>
2023-07-15 09:14:55 +0200

    Docstrings added

--- app.py
+++ app.py
@@ -6,17 +6,20 @@ app = Flask(__name__)
 
 
 def get_all_posts():
+    """read json file with blog posts and return the data"""
     with open("blog_posts.json", "r") as read_file:
         return json.load(read_file)
 
 
 def create_new_id():
+    """Create a new id for a new post based on the last id available in the json file"""
     blog_posts = get_all_posts()
     new_id = blog_posts[-1]["id"] + 1
     return new_id
 
 
 def fetch_post_by_id(post_id):
+    """Fetch a blog post with a specific id"""
     blog_posts = get_all_posts()
     fetched_post = {}
     for blog in blog_posts:
@@ -27,12 +30,15 @@ def fetch_post_by_id(post_id):
 
 @app.route('/')
 def index():
+    """Render index.html and show all blog posts,
+    with additional option to delete, update or add a blog post"""
     blog_posts = get_all_posts()
     return render_template('index.html', posts=blog_posts)
 
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
+    """Add a new blog post to the json file and redirects to index.html"""
     if request.method == 'POST':
         # Add the code that handles adding a new blog
         author = request.form.get("author")
@@ -58,6 +64,8 @@ def add():
 
 @app.route('/delete/<int:post_id>', methods=['GET', 'POST'])
 def delete(post_id):
+    """Gets a post_id, and deletes the blog post linked to the id from the json file.
+    Redirects to index.html after deletion"""
     updated_posts = []
     if request.method == 'POST':
         with open("blog_posts.json", "r") as read_file:
@@ -75,6 +83,8 @@ def delete(post_id):
 
 @app.route('/update/<int:post_id>', methods=['GET', 'POST'])
 def update(post_id):
+    """Gets a post_id, shows update.html where the title, author and content can be updated.
+    Saves the update in the json file and redirects to index.html"""
     post_to_update = fetch_post_by_id(post_id)
     if post_to_update is None:
         return "Post not found", 404
